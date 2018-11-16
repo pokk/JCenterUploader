66fc42e56a484aa62877753c7f87e956b69913d8
Jieyi <pokkbaby@gmail.com>
2018-11-16 11:39:36 +0900

    Update AndroidJacoco.gradle.kts

--- AndroidJacoco.gradle.kts
+++ AndroidJacoco.gradle.kts
@@ -20,9 +20,9 @@ tasks.withType<JacocoReport> {
                                    "excludes" to fileFilter))
     val mainSrc = "$project.projectDir/src/main/kotlin"
 
-    sourceDirectories = files(listOf(mainSrc))
-    classDirectories = files(listOf(debugTree))
-    executionData = fileTree(mapOf("dir" to project.buildDir,
-                                   "includes" to listOf("jacoco/testDebugUnitTest.exec",
-                                                        "outputs/code-coverage/connected/*coverage.ec")))
+    setSourceDirectories(files(listOf(mainSrc)))
+    setClassDirectories(files(listOf(debugTree)))
+    setExecutionData(fileTree(mapOf("dir" to project.buildDir,
+                               "includes" to listOf("jacoco/testDebugUnitTest.exec",
+                                                    "outputs/code-coverage/connected/*coverage.ec"))))
 }
